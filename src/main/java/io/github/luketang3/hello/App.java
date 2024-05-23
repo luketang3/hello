890e6432a72562511fe26a44ddd1622d08e95c2f
sff <tngjng2012@gmail.com>
2024-05-23 20:34:20 +0800

    init

---
+++ src/main/java/io/github/luketang3/hello/App.java
@@ -0,0 +1,11 @@
+package io.github.luketang3.hello;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+        System.out.println(Utils.hello("haha"));
+    }
+}
---
+++ src/main/java/io/github/luketang3/hello/Utils.java
@@ -0,0 +1,17 @@
+package io.github.luketang3.hello;
+
+/**
+ * Create by tangj
+ */
+public class Utils {
+
+    /**
+     * hello input
+     *
+     * @param input input file
+     * @return combine string
+     */
+    public static String hello(String input) {
+        return String.format("hello:%s", input);
+    }
+}
